3394cc0dbadb52f4237fbb03507ef782ff1c7c91
Jnaanendra Saurabh <vikramsaurabh14@gmail.com>
2017-04-03 21:25:29 +0530

    Update codeforces_sol.py

--- codeforces_sol.py
+++ codeforces_sol.py
@@ -6,23 +6,23 @@ def get_m_all(username,limit) :
     for idx in range(1,limit):
         page = requests.get('http://codeforces.com/submissions/'+username+'/page/'+str(idx))
         tree = html.fromstring(page.text)
-        verdict = tree.xpath('//*[@id="pageContent"]/div[4]/div[6]/table/tr[position() > 1]/td[6]/span/span/text()')
-        contestID = tree.xpath('//*[@id="pageContent"]/div[4]/div[6]/table/tr[position() > 1]/td[4]/a/text()')
-        problemID = tree.xpath('//*[@id="pageContent"]/div[4]/div[6]/table/tr[position() > 1]/td[1]/a/text()')
+        status = tree.xpath('//*[@id="pageContent"]/div[4]/div[6]/table/tr[position() > 1]/td[6]/span/span/text()')
+        contest_id = tree.xpath('//*[@id="pageContent"]/div[4]/div[6]/table/tr[position() > 1]/td[4]/a/text()')
+        problem_id = tree.xpath('//*[@id="pageContent"]/div[4]/div[6]/table/tr[position() > 1]/td[1]/a/text()')
 
-        for i in range(len(contestID)):
-            r = map(str,contestID[i].split())
-            contestID[i] = list(r)[0][:-1]
+        for i in range(len(contest_id)):
+            r = map(str,contest_id[i].split())
+            contest_id[i] = list(r)[0][:-1]
 
         k=1
 
         for i in range(len(verdict)):
-            if verdict[i] == 'Accepted':
-                solutionPage = requests.get('http://codeforces.com/contest/'+str(contestID[i])+'/submission/'+str(problemID[i]))
+            if status[i] == 'Accepted':
+                solutionPage = requests.get('http://codeforces.com/contest/'+str(contest_id[i])+'/submission/'+str(problem_id[i]))
                 #problemPage = requests.get('http://codeforces.com/problemset/problem/'+str(problemID[i]))
                 tree2 = html.fromstring(solutionPage.text)
                 code = tree2.xpath('//*[@id="pageContent"]/div[3]/pre/text()')
-                inp = 'solutions//p'+str(k)+'.txt' 
+                inp = 'p'+str(k)+'.txt' 
                 f=open(inp,'w')
                 sys.stdout = f
                 for j in code:
@@ -33,7 +33,7 @@ def get_m_all(username,limit) :
 
 print('enter the name of the user :')
 user = input()
-print('enter the limit of the page :')
+print('enter the limit of the pages you want to download solutions from :')
 limit = int(input())
 print('getting the solutions......')
 get_m_all(user,limit)
