6fa7d7500edd0e437b41e341eee7c56c69349cc9
Jnaanendra Saurabh <vikramsaurabh14@gmail.com>
2017-04-05 02:01:13 +0530

    Create codeforces_sol.py

---
+++ CFsol/codeforces_sol.py
@@ -0,0 +1,32 @@
+import requests
+from lxml import html
+import sys
+
+def get_m_all(username,limit) :
+    for idx in range(1,limit):
+        page = requests.get('http://codeforces.com/submissions/'+username+'/page/'+str(idx))
+        tree = html.fromstring(page.text)
+        status = tree.xpath('//*[@id="pageContent"]/div[4]/div[6]/table/tr[position() > 1]/td[6]/span/span/text()')
+        contest_id = tree.xpath('//*[@id="pageContent"]/div[4]/div[6]/table/tr[position() > 1]/td[4]/a/text()')
+        problem_id = tree.xpath('//*[@id="pageContent"]/div[4]/div[6]/table/tr[position() > 1]/td[1]/a/text()')
+
+        for i in range(len(contest_id)):
+            r = map(str,contest_id[i].split())
+            contest_id[i] = list(r)[0][:-1]
+
+        k=1
+
+        for i in range(len(verdict)):
+            if status[i] == 'Accepted':
+                solutionPage = requests.get('http://codeforces.com/contest/'+str(contest_id[i])+'/submission/'+str(problem_id[i]))
+                tree2 = html.fromstring(solutionPage.text)
+                code = tree2.xpath('//*[@id="pageContent"]/div[3]/pre/text()')
+                inp = 'solutions//p'+str(k)+'.txt' 
+                f=open(inp,'w')
+                sys.stdout = f
+                for j in code:
+                    j = j.rstrip('\r\n')
+                    print(j)
+                f.close()
+                k=k+1
+
